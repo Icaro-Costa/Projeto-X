ea1c3e147f9633616966be7fa91b461b89be8d2b
Icaro <163467778+Icaro-Costa@users.noreply.github.com>
2025-05-25 02:09:52 +0000

    Bonecos Frames
    Atk,Def

--- include/bonecos.h
+++ include/bonecos.h
@@ -5,4 +5,12 @@ void jogadorboneco(int x, int y);
 
 void inimigoboneco(int x, int y);
 
+void jogador_bonecoAtk(int x, int y);
+
+void inimigo_bonecoAtk(int x, int y);
+
+void inimigo_bonecoDef(int x, int y);
+
+void jogador_bonecoDef(int x, int y);
+
 #endif
